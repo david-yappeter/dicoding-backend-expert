2b17c95568958819d35231129d1753859235520f
David Yappeter <davidyap11les@gmail.com>
2022-11-28 19:59:02 +0700

    feat: Add MathBasic jest test

---
+++ 1. MathBasic Automation/src/MathBasic.js
@@ -0,0 +1,62 @@
+const isNumber = (v) => typeof v === 'number';
+
+const MathBasic = {
+  add: (...args) => {
+    if (args.length !== 2) {
+      throw new Error('fungsi add hanya menerima dua parameter');
+    }
+
+    const [a, b] = args;
+
+    if (!isNumber(a) || !isNumber(b)) {
+      throw new Error('fungsi hanya menerima parameter number');
+    }
+
+    return a + b;
+  },
+  subtract: (...args) => {
+    if (args.length !== 2) {
+      throw new Error('fungsi substract hanya menerima dua parameter');
+    }
+
+    const [a, b] = args;
+
+    if (!isNumber(a) || !isNumber(b)) {
+      throw new Error('fungsi hanya menerima parameter number');
+    }
+
+    return a - b;
+  },
+  multiply: (...args) => {
+    if (args.length !== 2) {
+      throw new Error('fungsi multiply hanya menerima dua parameter');
+    }
+
+    const [a, b] = args;
+
+    if (!isNumber(a) || !isNumber(b)) {
+      throw new Error('fungsi hanya menerima parameter number');
+    }
+
+    return a * b;
+  },
+  divide: (...args) => {
+    if (args.length !== 2) {
+      throw new Error('fungsi divide hanya menerima dua parameter');
+    }
+
+    const [a, b] = args;
+
+    if (b === 0) {
+      throw new Error('Divide By Zero Error');
+    }
+
+    if (!isNumber(a) || !isNumber(b)) {
+      throw new Error('fungsi hanya menerima parameter number');
+    }
+
+    return a / b;
+  },
+};
+
+module.exports = MathBasic;
